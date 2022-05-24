7c15ddfcc395da73358447bdc0f8c3817d1c0a02
Ampeire <ampeireedgar5@gmail.com>
2022-05-24 08:51:04 +0300

    Initial commit

---
+++ smsguru.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Plugin Name: SmsGuru
+ * Description: SMSGuru is an SMS plugin.
+ * Plugin URI: https://github.com/dancan-edgar/smsguru.git
+ * Author: Ampeire Edgar
+ * Version: 1.0.0
+ * Author URI: https://github.com/dancan-edgar
+ * License: GPL v2 or higher
+ *
+ * Text Domain: smsguru
+ *
+ * @package SmsGuru
+ * @category Core
+ *
+ *
+ * SmsGuru is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * SmsGuru is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+
+    if(! defined('ABSPATH')){
+        die; // Exit if accessed directly
+    }
+
